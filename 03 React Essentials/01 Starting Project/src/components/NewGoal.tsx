bdc07bc823e0010658f4a8994944c4462144e66d
Omkar Patil <git21cs072-t@students.git.edu>
2024-01-28 23:06:20 +0530

    new goal component started

--- 03 React Essentials/01 Starting Project/src/components/NewGoal.tsx
+++ 03 React Essentials/01 Starting Project/src/components/NewGoal.tsx
@@ -1,11 +1,30 @@
 
 import React from 'react'
+import { Goal } from '../App';
 
 
 
 const NewGoal = () => {
   return (
-    <div>NewGoal</div>
+    <form >
+      <p>
+        <label htmlFor='goal'>
+          Your Goal
+        </label>
+        <input type='text' id='goal'/>
+        
+      </p>
+      <p>
+        <label htmlFor='summary'>
+          Description
+        </label>
+        <input type='text' id='summary'/>
+        
+      </p>
+
+
+
+    </form>
   )
 }
 
