1b8829345308089ef5d4c9812bf6bb45eebdf7fc
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-24 15:23:39 +0530

    DONE

--- 04.Advanced Componrnts/04 Advanced Components/01 Starting Project/src/components/InfoBox.tsx
+++ 04.Advanced Componrnts/04 Advanced Components/01 Starting Project/src/components/InfoBox.tsx
@@ -1,9 +1,36 @@
 
 
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 
-export const InfoBox = () => {
-  return (
-    <div>InfoBox</div>
-  )
+
+type Hint = {
+  mode:'hint',
+  children:ReactNode
 }
+
+type Warning = {
+  mode:'warning',
+  severity:'low'|'medium'|'high';
+  children:ReactNode
+}
+type InfoboxProps = Hint | Warning;
+
+export const InfoBox:FC<InfoboxProps> = ({mode,children}) => {
+  if (mode==='hint') {
+    return (
+      <aside className='infobox infobox-hint'>
+        <p>{children}</p>
+
+
+      </aside>
+    )
+  }
+  return(
+    <aside className='infobox infobox-warning warning-medium '>
+      <h2>Warning</h2>
+        <p>{children}</p>
+      </aside>
+  )
+    
+  }
+  
