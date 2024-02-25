8908cdf6e2d4d5caa09e0bf0e681e9c676b05351
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-25 20:38:40 +0530

    sIMPLE WRAPPER Made!!!

--- 04.Advanced Componrnts/src/App.tsx
+++ 04.Advanced Componrnts/src/App.tsx
@@ -2,8 +2,10 @@ import { Input } from './Components/Input';
 function App() {
   return (<main>
 
-    <Input label='Enter name' id='name'/>
+    <Input label='Enter name' id='name' />
+
     <Input label='Enter age' id='age'/>
+
 
 
 
