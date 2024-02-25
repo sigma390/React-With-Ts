0386d5a2814cb8fc8deaf4fc204f97abc17248e3
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-25 22:15:08 +0530

    Done with easy Method!!!

--- 04.Advanced Componrnts/src/App.tsx
+++ 04.Advanced Componrnts/src/App.tsx
@@ -1,10 +1,18 @@
 import { Input } from './Components/Input';
+import { Button } from './Components/Button';
 function App() {
   return (<main>
 
     <Input label='Enter name' id='name' type='text' />
 
     <Input label='Enter age' id='age'/>
+
+    <p>
+      <Button el='button'>A button</Button>  
+    </p>
+    <p>
+      <Button el='anchor' href='https://github.com/'>A Link</Button>
+    </p>
 
 
 
--- 04.Advanced Componrnts/src/Components/Button.tsx
+++ 04.Advanced Componrnts/src/Components/Button.tsx
@@ -1,13 +1,30 @@
 
-import React from 'react'
+import React, { ComponentPropsWithoutRef } from 'react'
 
 
 
+type ButtonProps = 
+{
+    el:'button'
+} & ComponentPropsWithoutRef<'button'>
+
+type AnchorProps = 
+{
+    el:'anchor'
+} & ComponentPropsWithoutRef<'a'>
 
 
 
-export const Button = () => {
-  return (
-    <div>Button</div>
-  )
+type ButtProps = ButtonProps | AnchorProps;
+
+
+
+export const Button = (props:ButtProps) => {
+    if (props.el==='button') {
+        return<button className='button' {...props}>
+            
+            </button>
+        
+        }
+    return <a className='button' {...props}></a>
 }
