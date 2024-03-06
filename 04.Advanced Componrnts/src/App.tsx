995f671330b4e39a4e8cc155bf5f1468de08f8ec
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-06 23:19:42 +0530

    Done with ImperativeHandle

--- 04.Advanced Componrnts/src/App.tsx
+++ 04.Advanced Componrnts/src/App.tsx
@@ -3,22 +3,46 @@ import { useRef } from 'react';
 import { Button } from './Components/Button';
 import Container from './Components/Container';
 import Input from './Components/Input';
+import Form, { formMethods } from './Components/Form';
 function App() {
 
 
   // forward Ref Concept
 
   const input = useRef<HTMLInputElement>(null);
-
-
   const handleClick = ()=>{
     alert("Hii clicked here!!!!")
   }
 
+  // Form Wrapper Data Extraction
+
+  function handleSubmit(data:unknown){
+    const extractedData = data as {name:string,age:string};
+    console.log(extractedData);
+    //this function is called 
+    
+    formRefs.current?.clear();
+
+
+  }
+   // Use Imperative Concept
+   const formRefs = useRef<formMethods
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   >(null)
+
 
 
   return (<main>
-
+{/* 
     <Input label='Enter name' id='name' type='text' />
 
     <Input label='Enter age' id='age'/>
@@ -35,7 +59,17 @@ function App() {
 
     <Container as={Button} onClick={handleClick}>Click me and test</Container>
 
-    <Input label='text' id='test' ref={input}/>
+    <Input label='text' id='test' ref={input}/> */}
+   {/* custom form wrapper  */}
+    {/* <Form onSave={handleSubmit}  >  */}
+    <Form onSave={handleSubmit} ref={formRefs} > 
+      <Input type='text' label='Name' id='name' />
+      <Input type='number' label='age' id='age'/>
+      <p>
+        <Button >Save</Button>
+      </p>
+      
+    </Form>
 
 
 
--- 04.Advanced Componrnts/src/Components/Form.tsx
+++ 04.Advanced Componrnts/src/Components/Form.tsx
@@ -1,4 +1,5 @@
-import React, { ComponentPropsWithoutRef, FormEvent, useRef } from 'react'
+import React, { ComponentPropsWithoutRef, FormEvent, useImperativeHandle, useRef , forwardRef} from 'react'
+
 //its an wrapper Comnponent hence we Initiate Props
 
 type FormProps = ComponentPropsWithoutRef <'form'>&{
@@ -6,9 +7,68 @@ type FormProps = ComponentPropsWithoutRef <'form'>&{
 };
 
 // for useImperative we need to make a Type just to export Methods
-const Form = ({onSave,children,...otherprops}:FormProps) => {
+
+export type formMethods = {
+    clear:()=>void
+;}
+
+
+//====================> Noob <=====================
+
+// const  Form = (
+//     {onSave,children,...otherprops}:FormProps)=> {
+
+//     const form = useRef<HTMLFormElement>(null);
+
+    
+
+//     function handleSubmit(event:FormEvent<HTMLFormElement>){
+//         //prevent the default behavior of that submit
+//         event.preventDefault();
+//         const formData = new FormData(event.currentTarget); //inbuild JS FormData()
+//         console.log(formData)
+//         const data = Object.fromEntries(formData) // to extract Based upon {id};
+//         console.log(data)
+//         onSave(data);
+//         form.current?.reset();
+//     }
+//   return (
+//     <form onSubmit={handleSubmit} {...otherprops} ref={form}>
+//         {children}
+//     </form>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================> Pro <=========================
+
+
+
+const Form = forwardRef<formMethods,FormProps>(function Form(
+    {onSave,children,...otherprops}, ref) {
 
     const form = useRef<HTMLFormElement>(null);
+
+    //use imperative handle apply here
+    useImperativeHandle(ref,()=>{
+        return {clear:()=>{
+            console.log("Cleared control is here !!!")
+            form.current?.reset();
+        }}
+    })
 
     function handleSubmit(event:FormEvent<HTMLFormElement>){
         //prevent the default behavior of that submit
@@ -26,5 +86,5 @@ const Form = ({onSave,children,...otherprops}:FormProps) => {
     </form>
   )
 }
-
+)
 export default Form
