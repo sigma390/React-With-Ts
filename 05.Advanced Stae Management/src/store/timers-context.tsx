7d65f4d13713de3b7a05d306ab5d4ed24cc55692
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-08 12:46:39 +0530

    Step 1 done!!!

--- 05.Advanced Stae Management/src/store/timers-context.tsx
+++ 05.Advanced Stae Management/src/store/timers-context.tsx
@@ -1,1 +1,23 @@
 import { createContext } from "react";
+
+
+//typ timer
+
+type Timer = {
+    name:string,
+    duration:number
+}
+
+// Timer State
+type TimerState = {
+    isRunning:boolean,
+    timers:Timer[]
+}
+// type ContextValue
+type TimersContextValue = TimerState &{
+    addTimer:(timeData:Timer)=>void,
+    startTimers:()=>void,
+    stopTimers:()=>void;
+};
+
+const TimersContext = createContext<TimersContextValue | null>(null);
