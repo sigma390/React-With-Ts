67f9ad733b496436a34bed8c7240492013e50a42
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-08 13:06:10 +0530

    Created a Centralised Data!!!

--- 05.Advanced Stae Management/src/store/timers-context.tsx
+++ 05.Advanced Stae Management/src/store/timers-context.tsx
@@ -21,3 +21,31 @@ type TimersContextValue = TimerState &{
 };
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+// type for TimerContextValue 
+type TimerContextProviderProps = {
+    children:ReactNode
+}
+
+export default function TimersContextProvider({children}:TimerContextProviderProps){
+    //centralised data
+
+    const ctx:TimersContextValue = {
+        timers:[],
+        isRunning:false,
+        addTimer(timerData){
+            //..
+        },
+        startTimers() {
+            
+        },
+        stopTimers() {
+            
+        },
+
+    }
+    return(
+        // context object.Provider 
+        <TimersContext.Provider value={ctx} >{children}</TimersContext.Provider>
+    )
+};
