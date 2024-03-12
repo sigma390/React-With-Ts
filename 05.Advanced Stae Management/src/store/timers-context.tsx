58f4aba98c0e6edbbb5af66ced3c55992917ab5d
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-12 15:58:15 +0530

    Made a reducer function and made a type Action with help of lliteral

--- 05.Advanced Stae Management/src/store/timers-context.tsx
+++ 05.Advanced Stae Management/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { type ReactNode, createContext, useContext, useReducer } from "react";
 
 
 //typ timer
@@ -13,6 +13,15 @@ type TimerState = {
     isRunning:boolean,
     timers:Timer[]
 }
+
+//initial state for use reducer hook
+
+const initialState:TimerState={
+    isRunning:true,
+    timers:[]
+}
+
+
 // type ContextValue
 type TimersContextValue = TimerState &{
     addTimer:(timeData:Timer)=>void,
@@ -37,21 +46,60 @@ export function useTimersContext(){
 // type for TimerContextValue 
 type TimerContextProviderProps = {
     children:ReactNode
+}  
+
+// Action Props will be of type literal
+// action is message we are sending with dispatch
+
+type Action = {
+    type: 'ADD_TIMER' | 'START_TIMER' | 'STOP_TIMER'
 }
 
+
+
+
+// reducer 
+
+function timersReducer(state:TimerState,action:Action){
+
+    if (action.type==='START_TIMER') {
+        
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
 export default function TimersContextProvider({children}:TimerContextProviderProps){
+    
+    //useReducer Hook
+    const [timerState,dispatch] = useReducer(timersReducer, initialState);
+
+
+
+    
     //centralised data
 
     const ctx:TimersContextValue = {
         timers:[],
         isRunning:false,
         addTimer(timerData){
+            dispatch({type:'ADD_TIMER'});
             //..
         },
         startTimers() {
+            dispatch({type:'START_TIMER'});
             
         },
         stopTimers() {
+            dispatch({type:'STOP_TIMER'});
             
         },
 
